55ac4942eecfd8f1583a34ee0996d5ea6a912a10
maurya-ai-projects <mauryatt67@gmail.com>
2025-12-30 17:23:18 +0200

    Add files via upload

---
+++ Beginner-AI-Projects/Career-Recommendation-System/career_recommendation.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "010f6d89-9184-42c4-a9bb-195dc97a2618",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
